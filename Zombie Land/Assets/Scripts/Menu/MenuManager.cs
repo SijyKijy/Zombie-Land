82b22ff5149001997783653c346653df0bd5bd17
SijyKijy <SijyKijy@users.noreply.github.com>
2024-03-18 19:45:03 +0300

    Edit menu

--- Zombie Land/Assets/Scripts/Menu/MenuManager.cs
+++ Zombie Land/Assets/Scripts/Menu/MenuManager.cs
@@ -1,55 +1,31 @@
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    #region Singleton
-    private static MenuManager _default;
-    public static MenuManager Default => _default;
-
-    private void Awake()
-    {
-        _default = this;
-
-        Application.targetFrameRate = 60;
-
-        if (PlayerPrefs.GetInt(PREFS_FIRST_LAUNCH, 0) == 0)
-        {
-            PlayerPrefs.SetInt(PREFS_FIRST_LAUNCH, 1);
-            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 0, 1);
-            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 0 + PREFS_UPGRADE_NAME + 2, 1);
-            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 0 + PREFS_UPGRADE_NAME + 1, 3);
-            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 1, 1);
-            PlayerPrefs.SetInt(PREFS_SKIN_NAME + 0, 2);
-            //PlayerPrefs.SetInt("Money", 40000);
-            //PlayerPrefs.SetInt("Level", 5);
-            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 2, 1);
-            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 3, 1);
-            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 4, 1);
-            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 5, 1);
-            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 6, 1);
-        }
-
-        PlayerPrefs.SetInt("Weapon5", 0);
-    }
-    #endregion
-
-    [SerializeField]
-    private GameObject[] _stagesPool;
-    [SerializeField]
-    private LevelElement[] _levelElementPool;
-    [SerializeField]
-    private Curtain _curtain;
-
-    private Stack<GameObject> _stagesStack = new Stack<GameObject>();
     private const string
         PREFS_FIRST_LAUNCH = "FirstLaunch",
         PREFS_WEAPON_NAME = "Weapon",
         PREFS_UPGRADE_NAME = "Upgrade",
         PREFS_SKIN_NAME = "Skin",
         PREFS_LEVEL = "Level";
+
+    [SerializeField] private GameObject[] _stagesPool;
+
+    [SerializeField] private LevelElement[] _levelElementPool;
+
+    [SerializeField] private Curtain _curtain;
+
+    [SerializeField] private TMP_Text _address;
+
+    private readonly Stack<GameObject> _stagesStack = new();
 
     private void Start()
     {
@@ -62,9 +38,9 @@ public class MenuManager : MonoBehaviour
 
     private void ApplyLevelState()
     {
-        int targetLevel = PlayerPrefs.GetInt(PREFS_LEVEL, 0);
+        var targetLevel = PlayerPrefs.GetInt(PREFS_LEVEL, 0);
 
-        for (int i = 0; i < _levelElementPool.Length; i++)
+        for (var i = 0; i < _levelElementPool.Length; i++)
         {
             if (i == targetLevel)
             {
@@ -111,9 +87,11 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
-    
+
     public void HostGame()
     {
+        SetConnectionData();
+
         SceneManager.sceneLoaded += (scene, mode) =>
         {
             // Проверяем, что загружена нужная сцена
@@ -126,7 +104,7 @@ public class MenuManager : MonoBehaviour
             // Отписываемся от события
             //SceneManager.sceneLoaded -= OnSceneLoaded;
         };
-        
+
         _curtain.OnOpen += () =>
         {
             var asyncOperation = SceneManager.LoadSceneAsync(1);
@@ -134,9 +112,23 @@ public class MenuManager : MonoBehaviour
         };
         _curtain.OpenCurtain();
     }
-    
+
+    private void SetConnectionData()
+    {
+        var transport = NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+        if (transport is UnityTransport unityTransport)
+        {
+            var address = Regex.Replace(_address.text, "[^A-Za-z0-9.:]", "").Split(':');
+            var ip = IPAddress.Parse(address[0]).ToString();
+            var port = Convert.ToUInt16(address[1]);
+            unityTransport.SetConnectionData(ip, port);
+        }
+    }
+
     public void JoinGame()
     {
+        SetConnectionData();
+
         _curtain.OnOpen += () =>
         {
             var asyncOperation = SceneManager.LoadSceneAsync(1);
@@ -144,4 +136,36 @@ public class MenuManager : MonoBehaviour
         };
         _curtain.OpenCurtain();
     }
+
+    #region Singleton
+
+    public static MenuManager Default { get; private set; }
+
+    private void Awake()
+    {
+        Default = this;
+
+        Application.targetFrameRate = 60;
+
+        if (PlayerPrefs.GetInt(PREFS_FIRST_LAUNCH, 0) == 0)
+        {
+            PlayerPrefs.SetInt(PREFS_FIRST_LAUNCH, 1);
+            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 0, 1);
+            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 0 + PREFS_UPGRADE_NAME + 2, 1);
+            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 0 + PREFS_UPGRADE_NAME + 1, 3);
+            PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 1, 1);
+            PlayerPrefs.SetInt(PREFS_SKIN_NAME + 0, 2);
+            //PlayerPrefs.SetInt("Money", 40000);
+            //PlayerPrefs.SetInt("Level", 5);
+            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 2, 1);
+            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 3, 1);
+            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 4, 1);
+            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 5, 1);
+            //PlayerPrefs.SetInt(PREFS_WEAPON_NAME + 6, 1);
+        }
+
+        PlayerPrefs.SetInt("Weapon5", 0);
+    }
+
+    #endregion
 }
