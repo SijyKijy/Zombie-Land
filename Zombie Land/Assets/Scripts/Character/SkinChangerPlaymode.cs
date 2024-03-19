fee2d5daf7f3bf3edfdc86bee31266322bb2070d
SijyKijy <SijyKijy@users.noreply.github.com>
2024-03-20 01:49:52 +0300

    Skin sync login

--- Zombie Land/Assets/Scripts/Character/SkinChangerPlaymode.cs
+++ Zombie Land/Assets/Scripts/Character/SkinChangerPlaymode.cs
@@ -1,43 +1,55 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class SkinChangerPlaymode : MonoBehaviour
+public class SkinChangerPlaymode : NetworkBehaviour
 {
-    [SerializeField]
-    private List<GameObject> _skinPool;
-
     private const string PREFS_NAME = "Skin";
+
+    [SerializeField] private List<GameObject> _skinPool;
+
+    private readonly NetworkVariable<int> _currentSkin = new(-1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     private void Start()
     {
-        foreach (var skin in _skinPool)
-        {
-            skin.SetActive(false);
-        }        
-
-        for (int i = 0; i < _skinPool.Capacity; i++)
-        {
-            if(PlayerPrefs.GetInt(PREFS_NAME + i, 0) == 2)
-            {
-                _skinPool[i].SetActive(true);
-            }
-        }
+        var skinId = _currentSkin.Value == -1 ? GetSkinIdFromPrefs() : _currentSkin.Value;
+        UpdateSkin(skinId);
     }
 
-    public void UpdateSkin()
+    private int GetSkinIdFromPrefs()
+    {
+        for (var i = 0; i < _skinPool.Capacity; i++)
+        {
+            if (PlayerPrefs.GetInt(PREFS_NAME + i, 0) != 2) continue;
+            return i;
+        }
+
+        return 0;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            _currentSkin.Value = GetSkinIdFromPrefs();
+        }
+
+        _currentSkin.OnValueChanged += OnSkinChanged;
+        base.OnNetworkSpawn();
+    }
+
+    private void OnSkinChanged(int previousValue, int newValue)
+    {
+        UpdateSkin(newValue);
+    }
+
+    public void UpdateSkin(int skinId)
     {
         foreach (var skin in _skinPool)
         {
             skin.SetActive(false);
         }
 
-        for (int i = 0; i < _skinPool.Capacity; i++)
-        {
-            if (PlayerPrefs.GetInt(PREFS_NAME + i, 0) == 2)
-            {
-                _skinPool[i].SetActive(true);
-            }
-        }
+        _skinPool[skinId].SetActive(true);
     }
 }
